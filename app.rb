ab7b29aa782474c4fd21175266921499c8ada744
Sherry Seibel <sherry.seibel@gmail.com>
2014-06-13 18:53:50 -0400

    Add working sinatra app

--- app.rb
+++ app.rb
@@ -1,1 +1,15 @@
+require "sinatra"
+# require "active_record"
+# require "pg"
+#
+# ActiveRecord::Base.estabish_connection(
+#   adapter: "postgres"
+#   database: "flashcards"
+# )
+#
+# class Flashcard < ActiveRecord::BASE
+# end
 
+get "/" do
+  "Hello Avi!"
+end
