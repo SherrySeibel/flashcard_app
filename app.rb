eb3076456fc5a12d0e8b4f5f25110488801cdc74
Sherry Seibel <sherry.seibel@gmail.com>
2014-06-13 23:23:13 -0400

    WIP

--- app.rb
+++ app.rb
@@ -8,9 +8,21 @@ ActiveRecord::Base.establish_connection(
 )
 
 class Deck < ActiveRecord::Base
+  has_many :cards 
 end
 
+class Card < ActiveRecord::Base 
+end
 get "/" do
   @decks = Deck.all
   erb :index 
+end
+#
+# get "/decks/new" do
+#   erb :new_deck
+# end
+
+get "/decks/:id" do
+  @deck = Deck.find(params[:id])
+  @cards = 
 end
